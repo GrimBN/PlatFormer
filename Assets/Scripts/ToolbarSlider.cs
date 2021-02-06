2675b479ea7e1cc11a3b56f376bd6a421392ec7a
grimbn <bhasin.agrim996@gmail.com>
2021-02-06 15:22:42 +0530

    Toolbar code optimization

--- Assets/Scripts/ToolbarSlider.cs
+++ Assets/Scripts/ToolbarSlider.cs
@@ -5,54 +5,17 @@ using UnityEngine.UI;
 
 public class ToolbarSlider : MonoBehaviour
 {
-    [SerializeField] float maxXMultiplier, lerpValue;
-    //[SerializeField] GameObject blockingCollider;
-    [SerializeField] GameObject toolbarParentObject;
-    float differenceMultiplier, objectDifferenceMultiplier, minColliderXValue, maxColliderXValue, minObjectXValue, maxObjectXValue;
+    [SerializeField] float  lerpValue;    
     Slider slider;
     Sprite flippedArrowSprite;
     Image childArrowImage;
 
     private void Start()
-    {        
-        maxObjectXValue = Screen.width * maxXMultiplier;
-        minObjectXValue = toolbarParentObject.transform.position.x;
-        //minColliderXValue = Camera.main.ScreenToWorldPoint(new Vector3(minObjectXValue, 0, blockingCollider.transform.position.z)).x;
-        //maxColliderXValue = Camera.main.ScreenToWorldPoint(new Vector3(maxObjectXValue, 0, blockingCollider.transform.position.z)).x;
-        //blockingCollider.transform.position = new Vector2(minColliderXValue, blockingCollider.transform.position.y);
-        //differenceMultiplier = maxColliderXValue - minColliderXValue;        
-        objectDifferenceMultiplier = maxObjectXValue - minObjectXValue;
-
+    {
         flippedArrowSprite = Resources.Load<Sprite>("FlippedArrow");
         childArrowImage = gameObject.GetComponentInChildren<Image>();
-        slider = GetComponent<Slider>();      
-        UpdatePositions(slider.value);
+        slider = GetComponent<Slider>();
     }
-
-    public void UpdatePositions(float value)
-    {
-        //UpdateColliderPos(value);
-        UpdateParentObjectPos(value);
-        //StartCoroutine(OpenSlider());
-    }
-
-    /*private void UpdateColliderPos(float value)
-    {
-        if(value <=1 && value >=0)
-        {
-            Vector2 newColliderPos = new Vector2(minColliderXValue + value * differenceMultiplier, blockingCollider.transform.position.y);
-            blockingCollider.transform.position = newColliderPos;
-        }
-    }*/
-
-    private void UpdateParentObjectPos(float value)
-    {
-        if (value <= 1 && value >= 0)
-        {            
-            Vector2 newObjectPos = new Vector2(minObjectXValue + value * objectDifferenceMultiplier, toolbarParentObject.transform.position.y);
-            toolbarParentObject.transform.position = newObjectPos;
-        }
-    }    
 
     public void StartSliderCoroutine()
     {
@@ -66,9 +29,7 @@ public class ToolbarSlider : MonoBehaviour
             childArrowImage.overrideSprite = flippedArrowSprite;
             while (slider.value > 0.01)
             {
-                slider.value = Mathf.Lerp(0, slider.value, 1-lerpValue);
-                //UpdateColliderPos(slider.value);
-                UpdateParentObjectPos(slider.value);
+                slider.value = Mathf.Lerp(0, slider.value, 1-lerpValue);                                
                 yield return null;
             }
             
@@ -79,9 +40,7 @@ public class ToolbarSlider : MonoBehaviour
             childArrowImage.overrideSprite = null;
             while (slider.value < 0.99)
             {
-                slider.value = Mathf.Lerp(slider.value, 1, lerpValue);
-                //UpdateColliderPos(slider.value);
-                UpdateParentObjectPos(slider.value);
+                slider.value = Mathf.Lerp(slider.value, 1, lerpValue);                                
                 yield return null;
             }
             
