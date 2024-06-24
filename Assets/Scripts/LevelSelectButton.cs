d8457f7e4a6b541ee68f0057deebfed832322ba3
Agrim Bhasin <bhasin.agrim996@gmail.com>
2024-06-25 02:13:18 +0530

    Updated required visual studio package + created LevelSelectButton.cs (unused) + cleaned up stuff in level select scene

---
+++ Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteAlways]
+public class LevelSelectButton : MonoBehaviour
+{
+    [SerializeField] private Button buttonComponent;
+    [SerializeField] private TextMeshProUGUI buttonText;
+
+    private void OnGUI() 
+    {
+        buttonComponent = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+
+        int levelNumber = transform.GetSiblingIndex() + 1;
+        buttonText.text = levelNumber.ToString();
+
+    }
+
+    private void Start()
+    {
+        LevelLoader levelLoader = FindObjectOfType<LevelLoader>();
+
+        buttonComponent.onClick.AddListener(() => levelLoader.LoadGivenScene(transform.GetSiblingIndex() + 1));
+        
+    }
+}
