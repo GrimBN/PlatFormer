3b43713c0488c70492d92f0eeaa212d5d3648b65
Agrim Bhasin <bhasin.agrim996@gmail.com>
2024-07-26 22:51:40 +0530

    Created simple script for arrow button tutorials + Corrected level 1 valid area colliders

---
+++ Assets/Scripts/ArrowTutorialAdvancer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowTutorialAdvancer : MonoBehaviour
+{
+    private bool arrowClicked;
+    [SerializeField] private GameObject tutorialBox;
+    private Button arrowButton;
+
+    void Start()
+    {
+        arrowClicked = false;
+        arrowButton = GetComponent<Button>();
+        arrowButton.interactable = false;
+    }
+
+    public void ArrowClicked()
+    {
+        if(!arrowClicked)
+        {
+            arrowClicked = true;
+            tutorialBox.SetActive(true);
+            arrowButton.interactable = true;
+        }
+    }
+
+
+
+    
+}
